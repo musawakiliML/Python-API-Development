0a670752180c2c50f60571dd2bcd57d9fe35b2b8
musawakiliML <musawakiliml@hotmail.com>
2022-08-21 18:20:45 +0100

    created a simple API

--- API Using FastAPI/main.py
+++ API Using FastAPI/main.py
@@ -3,6 +3,14 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
 app = FastAPI()
 
 # main page
@@ -12,8 +20,26 @@ app = FastAPI()
 async def root():
     return {'message': 'Hello World!'}
 
-# get item id path
+# post item
 
+
+@app.post('/items/')
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({'price_with_tax': price_with_tax})
+    return item_dict
+
+# put item
+
+
+@app.put('/items/{item_id}')
+async def create_item_id(item_id: int, item: Item):
+    return {'item_id': item_id, **item.dict()}
+
+
+# get item id path
 
 @app.get('/items/{item_id}')
 async def read_item(item_id: int):
@@ -31,4 +57,5 @@ async def read_user_me():
 
 @app.get('/users/{user_id}')
 async def read_user(user_id: str):
+    '''Read user Id'''
     return {'user_id': user_id}
