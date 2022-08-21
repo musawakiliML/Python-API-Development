a33d464cc879da9d4270e12735b5575785694c80
musawakiliML <musawakiliml@hotmail.com>
2022-08-21 17:01:38 +0100

    Update main.py and new requirements.txt

--- API Using FastAPI/main.py
+++ API Using FastAPI/main.py
@@ -1,7 +1,34 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# main page
+
+
 @app.get('/')
 async def root():
-    return {'message':'Hello World!'}
+    return {'message': 'Hello World!'}
+
+# get item id path
+
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    return {'item_id': item_id}
+
+# get current user path
+
+
+@app.get('/users/me')
+async def read_user_me():
+    return {'user_id': "the current user"}
+
+# get user by id
+
+
+@app.get('/users/{user_id}')
+async def read_user(user_id: str):
+    return {'user_id': user_id}
